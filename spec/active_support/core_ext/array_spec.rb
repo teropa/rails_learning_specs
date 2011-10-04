87b6600916e4e36ba306925741162c5fd43144d9
Tero Parviainen <tero.parviainen@eficode.fi>
2011-10-04 18:23:00 +0300

    array grouping

--- spec/active_support/core_ext/array_spec.rb
+++ spec/active_support/core_ext/array_spec.rb
@@ -109,4 +109,108 @@ describe "ActiveSupport Array extensions" do
     
   end
   
+  describe "grouping to even sizes" do
+    
+    it "splits to groups of n" do
+      res = [1, 2, 3, 4].in_groups_of(2)
+      res.should == [[1, 2], [3, 4]]
+    end
+    
+    it "yields the groups if a block is given" do
+      res = []
+      [1, 2, 3, 4].in_groups_of(2) { |group| res << group }
+      res.should == [[1, 2], [3, 4]]
+    end
+    
+    it "pads the last group if a fill argument is given" do
+      res = [1, 2, 3, 4].in_groups_of(3, :fill)
+      res.should == [[1, 2, 3], [4, :fill, :fill]]
+    end
+    
+    it "does not pad the last group if the split is even" do
+      res = [1, 2, 3, 4].in_groups_of(2, :fill)
+      res.should == [[1, 2], [3, 4]]
+    end
+    
+    it "pads with nil if no fill argument is given" do
+      res = [1, 2, 3, 4].in_groups_of(3)
+      res.should == [[1, 2, 3], [4, nil, nil]]
+    end
+    
+  end
+  
+  describe "grouping to a number of groups" do
+    
+    it "splits to n groups of size/n when split evenly" do
+      res = [1, 2, 3, 4].in_groups(2)
+      res.should == [[1, 2], [3, 4]]
+    end
+
+    it "returns n empty arrays if grouping an empty array" do
+      [].in_groups(2).should == [[], []]
+    end
+    
+    it "yields the groups if a block is given" do
+      res = []
+      [1, 2, 3, 4].in_groups(2) { |group| res << group }
+      res.should == [[1, 2], [3, 4]]
+    end
+    
+    it "pads the last group when a fill argument is given" do
+      res = [1, 2, 3].in_groups(2, :fill)
+      res.should == [[1, 2], [3, :fill]]
+    end
+    
+    it "pads with nil when a fill argument is not given" do
+      res = [1, 2, 3].in_groups(2)
+      res.should == [[1, 2], [3, nil]]
+    end
+    
+    
+    it "distributes pad evenly when not split evenly" do
+      res = [1, 2, 3, 4].in_groups(3)
+      res.should == [[1, 2], [3, nil], [4, nil]]
+    end
+    
+    it "returns groups with pad only when splitting to larger number than the original array" do
+      res = [1, 2, 3, 4].in_groups(6)
+      res.should == [[1], [2], [3], [4], [nil], [nil]]
+    end
+    
+  end
+  
+  describe "splitting" do
+    
+    it "splits from match" do
+      res = [1, 2, 3, 4, 5].split(3)
+      res.should == [[1, 2], [4, 5]]
+    end
+    
+    it "splits from multiple matches" do
+      res = [1, 2, 3, 4, 5, 4, 3, 2, 1].split(3)
+      res.should == [[1, 2], [4, 5, 4], [2, 1]]
+    end
+    
+    it "splits by testing block if block is given" do
+      res = [1, 2, 3].split { |n| n % 2 == 0 }
+      res.should == [[1], [3]]
+    end
+    
+    it "returns an empty array as last if the last item matches" do
+      res = [1, 2, 3, 4].split(4)
+      res.should == [[1, 2, 3], []]
+    end
+    
+    it "returns an empty array as first if the first item matches" do
+      res = [1, 2, 3, 4].split(1)
+      res.should == [[], [2, 3, 4]]
+    end
+    
+    it "does not split when there is no match" do
+      res = [1, 2, 3, 4].split(5)
+      res.should == [[1, 2, 3, 4]]
+    end
+    
+  end
+  
 end
